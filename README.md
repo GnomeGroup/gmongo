1746458d764394eb3702ad85dbc37d6a7404e76f
Encke Technologies <james@encke.com>
2020-07-12 03:14:37 -0600

    NK Mongo Version

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # NK-Mongo
 MongoDB Connection Class for the NK Node Package
+
+## Installation
+
+Install using NPM
+
+```bash
+npm i nk-mongo --save
+```
+
+## How to use
+
+
+### Start and connect to server
+```node
+const NKMongo = require( 'nk-mongo' )
+//                  dbName,         ip,   port, callback
+NKMongo.start( 'MyDatabase', '127.0.0.1', 27017, () => {
+  //                    dbName, table,      rowOrRows,                                                                        callback
+  NKMongo.insert( 'MyDatabase', 'users', { username: 'jose', pass: '123', active: false, added: ( new Date() ).getTime() }, () => console.log( 'all done' ) )
+  //                    dbName, table,     dataToRemove,                      callback
+  NKMongo.delete( 'MyDatabase', 'users', { myuser: NKMongo.id( user._id ) }, () => console.log( 'all done' ) )
+  //                    dbName, table,      dataToUpdate,       newData,        callback
+  NKMongo.update( 'MyDatabase', 'users', { active: false }, { active: true }, () => console.log( 'all done' ) )
+  //                        dbName, table,      query,                              callback
+  NKMongo.singleQuery( 'MyDatabase', 'users', { myuser: NKMongo.id( user._id ) }, rowFromQuery => console.log( rowFromQuery ) )
+  //                  dbName, table,      query,          callback
+  NKMongo.query( 'MyDatabase', 'users', { active: true }, rowsFromQuery => console.log( rowsFromQuery ) )
+  //dbName, table, tableIDField, joinTo, joinToIDField, joinedToElement, sortBy, query, callback
+  NKMongo.join( 'MyDatabase', 'users', tableIDField, joinTo, joinToIDField, joinedToElement, sortBy, query, rowsFromQuery => console.log( rowsFromQuery ) )
+  //                      dbName, table,    joins, max,   sortBy,       query,            callback
+  NKMongo.joinsLimit( 'MyDatabase', 'users', joins, 100, { added: 1 }, { active: true }, rowsFromQuery => console.log( rowsFromQuery ) )
+  //                      dbName, table,    sortBy,       query,              callback
+  NKMongo.querySort( 'MyDatabase', 'users', { added: 1 }, { active: true }, rowsFromQuery => console.log( rowsFromQuery ) )
+  //                      dbName,       table,  max,  sortBy,       query,            callback
+  NKMongo.queryLimitSort( 'MyDatabase', 'users', 100, { added: 1 }, { active: true }, rowsFromQuery => console.log( rowsFromQuery ) )
+})
+```
---
+++ mongo.js
@@ -0,0 +1,130 @@
+const mongo		=	require( 'mongodb' ).MongoClient
+const objectid	=	require( 'mongodb' ).ObjectID
+
+const mongoDBJSObject = {
+	db: null,
+	retryTimeout: 100,
+	insertList: {},
+	deleteList: {},
+	updateList: {},
+	databaseList: {},
+	id: name => objectid( name ),
+	start: ( dbName, ip, port, callback ) => mongo.connect( ( 'mongodb://' + ip + ':' + port + '/' + dbName ), { useNewUrlParser: true, useUnifiedTopology: true }, ( err, db ) => {
+		if( !err && db )	{
+			mongoDBJSObject.databaseList[dbName] = db.db( dbName )
+			mongoDBJSObject.databaseList[dbName].collection( dbName )
+			if( callback )	{
+				callback()
+			}
+		}	else	{
+			console.log( 'Database Connection Error: ' + err )
+		}
+	}),
+	insert: ( dbName, table, rowOrRows, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			if( mongoDBJSObject.insertList[table] )	{
+				setTimeout( () => mongoDBJSObject.insert( dbName, table, rowOrRows, callback ), mongoDBJSObject.retryTimeout )
+			}	else	{
+				mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => {
+					if( Array.isArray( rowOrRows ) )	{
+						mongoDBJSObject.insertList[table] = 1
+						collection.insertMany( rowOrRows, () => mongoDBJSObject.insertDone( table, callback ) )
+					}	else	{
+						mongoDBJSObject.insertList[table] = 1
+						collection.insertOne( rowOrRows, () => mongoDBJSObject.insertDone( table, callback ) )
+					}
+				})
+			}
+		}
+	},
+	insertDone: ( table, callback ) => {
+		if( mongoDBJSObject.insertList[table] )	{
+			mongoDBJSObject.insertList[table]--
+			if( mongoDBJSObject.insertList[table] < 1 )	{
+				delete mongoDBJSObject.insertList[table]
+			}
+		}
+		if( callback )	{
+			callback()
+		}
+	},
+	delete: ( dbName, table, dataToRemove, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			if( mongoDBJSObject.deleteList[table] )	{
+				setTimeout( () => mongoDBJSObject.delete( dbName, table, dataToRemove, callback ), mongoDBJSObject.retryTimeout )
+			}	else	{
+				mongoDBJSObject.deleteList[table] = true;
+				mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => collection.deleteMany( dataToRemove, () => mongoDBJSObject.deleteDone( table, callback ) ) )
+			}
+		}
+	},
+	deleteDone: ( table, callback ) => {
+		if( mongoDBJSObject.deleteList[table] )	{
+			delete mongoDBJSObject.deleteList[table]
+		}
+		if( callback )	{
+			callback()
+		}
+	},
+	update: ( dbName, table, dataToUpdate, newData, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			if( mongoDBJSObject.updateList[table] )	{
+				setTimeout( () => mongoDBJSObject.update( dbName, table, dataToUpdate, newData, callback ), mongoDBJSObject.retryTimeout )
+			}	else	{
+				mongoDBJSObject.updateList[table] = true
+				mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => collection.updateMany( dataToUpdate, { $set: newData }, () => mongoDBJSObject.updateDone( table, callback ) ) )
+			}
+		}
+	},
+	updateDone: ( table, callback ) => {
+		if( mongoDBJSObject.updateList[table] )	{
+			delete mongoDBJSObject.updateList[table]
+		}
+		if( callback )	{
+			callback()
+		}
+	},
+	singleQuery: ( dbName, table, query, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => collection.findOne( query, ( err, item ) => callback( item ) ) );
+		}
+	},
+	query: ( dbName, table, query, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => collection.find( query ).toArray( ( err, items ) => callback( items ) ) );
+		}
+	},
+	join: ( dbName, table, tableIDField, joinTo, joinToIDField, joinedToElement, sortBy, query, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => {
+				let lkData = { $lookup: { from: joinTo, localField: tableIDField, foreignField: joinToIDField, as: joinedToElement } }
+				collection.aggregate( [ lkData, { $match: query } ], { $sort: sortBy } ).toArray( ( err, items ) => callback( items ) )
+			});
+		}
+	},
+	joinsLimit: ( dbName, table, joins, max, sortBy, query, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => {
+				let lookupdata = []
+				for( let i = 0; i < joins.length; i++ )	{
+					let lookupDataItem = { from: joins[i].from, localField: joins[i].field, foreignField: joins[i].fromField, as: joins[i].name }
+					lookupdata.push( { $lookup: lookupDataItem } )
+				}
+				lookupdata.push( { $match: query } )
+				collection.aggregate( lookupdata, { $sort: sortBy, $limit: max } ).toArray( ( err, items ) => callback( items ) )
+			})
+		}
+	},
+	querySort: ( dbName, table, sortBy, query, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => collection.find( query ).sort( sortBy ).toArray( ( err, items ) => callback( items ) ) )
+		}
+	},
+	queryLimitSort: ( dbName, table, max, sortBy, query, callback ) => {
+		if( mongoDBJSObject.databaseList[dbName] )	{
+			mongoDBJSObject.databaseList[dbName].collection( table, ( err, collection ) => collection.find( query ).limit( max ).sort( sortBy ).toArray( ( err, items ) => callback( items ) ) )
+		}
+	}
+}
+
+module.exports = mongoDBJSObject
